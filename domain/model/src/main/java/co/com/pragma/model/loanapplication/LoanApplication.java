51f21870ede46b4e74189b3647f763dc5d3fc2f7
DimasHernandez <dimas.hernandez@utp.edu.co>
2025-08-26 16:29:21 -0500

    generate models and restConsumer

---
+++ domain/model/src/main/java/co/com/pragma/model/loanapplication/LoanApplication.java
@@ -0,0 +1,12 @@
+package co.com.pragma.model.loanapplication;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class LoanApplication {
+}
